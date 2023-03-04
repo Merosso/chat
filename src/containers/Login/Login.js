595b086f1ad202119b977ff0d1d89141c0da09f7
Maxim <prabol@yandex.ru>
2023-03-04 20:08:07 +0300

    created login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
+import {Link, useNavigate} from 'react-router-dom'
+import {auth} from '../../firebase'
+import {onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth'
 
 const Login = () => {
+    const [err, setErr] = useState(false)
+    const navigate = useNavigate()
+
+    const loginUser = async (e) => {
+        e.preventDefault()
+
+        const email = e.target[0].value
+        const password = e.target[1].value
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            onAuthStateChanged(auth, (user) => {
+                localStorage.setItem('token', user.accessToken)
+            })
+            navigate('/')
+        } catch (err) {
+            setErr(true)
+        }
+    }
 
     return (
         <div className={'login'}>
             <div className={'login__panel'}>
                 <span className={'login__panel_logo'}>Chat</span>
-                <span className={'login__panel_description'}>Login</span>
+                <span className={'login__panel_description'} onSubmit={loginUser}>Login</span>
                 <form className={'login__panel_form'}>
                     <input type={'email'} name={'email'} placeholder={'Email'}/>
                     <input type={'password'} name={'password'} placeholder={'Password'}/>
                     <button className={'login__panel_button'}>Login</button>
                 </form>
-                <span>You don't have an account? <p>Register</p></span>
+                <span>You don't have an account? <Link to={'/signup'}>Register</Link></span>
             </div>
         </div>
     )
--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Register.css'
 import {Link, useNavigate} from 'react-router-dom'
 import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
